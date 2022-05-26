c5009e6556a82358816ff81aaf69d8d85ca69ea5
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-27 01:10:11 +0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import NewPlace from "./places/components/NewPlace";
+import Users from "./user/components/Users";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/new/place">
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/places/components/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h1>New Places Page</h1>;
+};
+
+export default NewPlace;
