c35a0c4e963dc3a353795dd43c04acb9c814e1b4
Maxpayne <ferhatadibelli9@gmail.com>
2022-06-06 23:24:48 +0300

    custom button created

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,27 +1,26 @@
 import React from "react";
 import Card from "../../shared/components/UIElement/Card";
+import Button from "../../shared/components/FormElements/Button"
 import "./PlaceItem.css";
 
-const PlaceItem = (props) => {
-  return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <p>{props.address}</p>
-          <h3>{props.description}</h3>
-        </div>
-        <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
-        </div>
-      </Card>
-    </li>
-  );
-};
+const PlaceItem = (props) => (
+  <li className="place-item">
+    <Card className="place-item__content">
+      <div className="place-item__image">
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className="place-item__info">
+        <h2>{props.title}</h2>
+        <p>{props.address}</p>
+        <h3>{props.description}</h3>
+      </div>
+      <div className="place-item__actions">
+        <Button>VIEW ON MAP</Button>
+        <Button>EDIT</Button>
+        <Button>DELETE</Button>
+      </div>
+    </Card>
+  </li>
+);
 
 export default PlaceItem;
