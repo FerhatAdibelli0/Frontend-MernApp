f85f73cd1d4deea65b8e71cbee1175e019c0c262
Maxpayne <ferhatadibelli9@gmail.com>
2022-06-07 23:51:17 +0300

    places increased

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -29,6 +29,19 @@ const DUMMY_PLACES = [
     },
     description: "One of the most famous sky scrapers in the world!",
   },
+  {
+    id: "p1",
+    creator: "u3",
+    title: "Empire State Building",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg",
+    coordinator: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    description: "One of the most famous sky scrapers in the world!",
+  },
 ];
 
 const UserPlaces = () => {
