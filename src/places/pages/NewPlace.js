8e291f871c14fdc9ce28c0d5d6825660725e890c
Maxpayne <ferhatadibelli9@gmail.com>
2022-06-11 22:32:49 +0300

    input added

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h1>New Places Page</h1>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title"/>
+    </form>
+  );
 };
 
 export default NewPlace;
